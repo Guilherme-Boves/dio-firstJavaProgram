d0690e2d3dd676026dbf6d2936cef4f7ffb21593
Guilherme-Boves <ghboves@gmail.com>
2022-02-16 11:32:50 -0300

    Métodos para calcular taxa adicionados

--- src/main/java/application/aplic.java
+++ src/main/java/application/aplic.java
@@ -5,7 +5,8 @@ public class aplic {
 
 	public static void main(String[] args) {
 		
-		Order order = new Order("1234");
+		Order order = new Order("1234", 200);		
+		order.setTotalValue(order.calculateFee());
     	System.out.println(order);
 
 	}
--- src/main/java/entities/Order.java
+++ src/main/java/entities/Order.java
@@ -2,14 +2,66 @@ package entities;
 
 public class Order {
 	
-	private final String code;
+	private final String code;	
+	private int totalValue;
+	//private double totalValue;
 	
-	public Order(String code) {
-		this.code = code; 
+	
+	// totalValue = Double
+	
+	/*public Order(String code, double totalValue) {
+		this.code = code;
+		this.totalValue = totalValue;
+	}*/	
+	
+	/*public double getTotalValue() {
+		return totalValue;
+	}*/
+	
+	/*public void setTotalValue(double totalValue) {
+		this.totalValue = totalValue;
+	}*/
+	
+	// Calculando taxa através do condicional IF	
+		/*public double calculateFee() {
+			if (getTotalValue() > 100.0) {
+				return getTotalValue() * 0.90;
+			} else {
+				return getTotalValue();
+			}		
+		}*/
+	
+	
+	// totalValue = int
+	
+	public Order(String code, int totalValue) {
+		this.code = code;
+		this.totalValue = totalValue;
 	}
+	
+	public int getTotalValue() {
+		return totalValue;
+	}
+	
+	public void setTotalValue(int totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	// Calculando taxa através do condicional Switch Case	
+	public int calculateFee() {
+		switch (getTotalValue()) {
+			case 100:
+				return getTotalValue() * 2;
+			case 200:
+				return getTotalValue() * 3;
+			default:
+				return getTotalValue();
+ 		}
+	}
+	
 	
 	@Override
 	public String toString() {
-		return "Order = {"+ "code: " + code + "'" + "}";		
+		return "Order = {"+ "code: " + code + " value: " + getTotalValue() + "}";		
 	}
 }
