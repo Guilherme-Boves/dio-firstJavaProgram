5a2a99f2cd332f242704fe549d0028eb00528a9c
Guilherme-Boves <ghboves@gmail.com>
2022-02-16 10:24:17 -0300

    First Java Program

---
+++ src/main/java/application/aplic.java
@@ -0,0 +1,12 @@
+package application;
+import entities.Order;
+
+public class aplic {
+
+	public static void main(String[] args) {
+		
+		Order order = new Order("1234");
+    	System.out.println(order);
+
+	}
+}
---
+++ src/main/java/entities/Order.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class Order {
+	
+	private final String code;
+	
+	public Order(String code) {
+		this.code = code; 
+	}
+	
+	@Override
+	public String toString() {
+		return "Order = {"+ "code: " + code + "'" + "}";		
+	}
+}
